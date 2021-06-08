4b202b566c9a68891042a371f6dfe1fee4093fe5
h4x3rotab <h4x3rotab@gmail.com>
2021-06-08 09:19:17 +0000

    script: update export-parachain-genesis.sh

--- scripts/export-parachain-genesis.sh
+++ scripts/export-parachain-genesis.sh
@@ -7,3 +7,7 @@ mkdir -p tmp/kernels
 
 ./target/release/khala-node export-genesis-wasm -r --chain khala-local-2004 > ./tmp/kernels/local-genesis.wasm
 ./target/release/khala-node export-genesis-state --chain khala-local-2004 > ./tmp/kernels/local-state.hex
+
+./target/release/khala-node export-genesis-wasm -r --chain khala-local-2000 > ./tmp/kernels/local-genesis-2000.wasm
+./target/release/khala-node export-genesis-state --chain khala-local-2000 > ./tmp/kernels/local-state-2000.hex
+
