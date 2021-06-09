a0bbfb6e3c5fa3e37c060ec0b25e344fee4f9411
h4x3rotab <h4x3rotab@gmail.com>
2021-06-09 06:25:03 +0000

    script: update export-parachain-genesis.sh

--- scripts/export-parachain-genesis.sh
+++ scripts/export-parachain-genesis.sh
@@ -2,12 +2,14 @@
 
 mkdir -p tmp/kernels
 
-./target/release/khala-node export-genesis-wasm -r --chain khala-staging > ./tmp/kernels/staging-genesis.wasm
+./target/release/khala-node export-genesis-wasm -r --chain khala-staging > ./tmp/kernels/staging-code.wasm
 ./target/release/khala-node export-genesis-state --chain khala-staging > ./tmp/kernels/staging-state.hex
 
-./target/release/khala-node export-genesis-wasm -r --chain khala-local-2004 > ./tmp/kernels/local-genesis.wasm
+./target/release/khala-node export-genesis-wasm -r --chain khala-local-2004 > ./tmp/kernels/local-code.wasm
 ./target/release/khala-node export-genesis-state --chain khala-local-2004 > ./tmp/kernels/local-state.hex
 
-./target/release/khala-node export-genesis-wasm -r --chain khala-local-2000 > ./tmp/kernels/local-genesis-2000.wasm
+./target/release/khala-node export-genesis-wasm -r --chain khala-local-2000 > ./tmp/kernels/local-code-2000.wasm
 ./target/release/khala-node export-genesis-state --chain khala-local-2000 > ./tmp/kernels/local-state-2000.hex
 
+./target/release/khala-node export-genesis-wasm -r --chain whala > ./tmp/kernels/whala-code.wasm
+./target/release/khala-node export-genesis-state --chain whala > ./tmp/kernels/whala-state.hex
