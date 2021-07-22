65f38a9c7c5f1bdd4a919b9a7d788d208e308eea
jasl <jasl9187@hotmail.com>
2021-07-22 22:30:19 +0800

    bump missing spec_version

--- runtime/khala/src/lib.rs
+++ runtime/khala/src/lib.rs
@@ -109,7 +109,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("khala"),
     impl_name: create_runtime_str!("khala"),
     authoring_version: 1,
-    spec_version: 8,
+    spec_version: 9,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,
