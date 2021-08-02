f19abe0033505e7b427865392a66adcaf29c3e55
jasl <jasl9187@hotmail.com>
2021-08-02 21:47:55 +0800

    correct candidacy_bond config

--- runtime/khala/src/lib.rs
+++ runtime/khala/src/lib.rs
@@ -451,7 +451,7 @@ impl pallet_timestamp::Config for Runtime {
 }
 
 parameter_types! {
-    pub const ExistentialDeposit: Balance = EXISTENTIAL_DEPOSIT;
+    pub const ExistentialDeposit: Balance = 1 * CENTS; // 0.01 PHA
     // For weight estimation, we assume that the most locks on an individual account will be 50.
     // This number may need to be adjusted in the future if this assumption no longer holds true.
     pub const MaxLocks: u32 = 50;
