a0ef7216c9198ca21092c1ff2aeb9fdd467011e5
jasl <jasl9187@hotmail.com>
2021-08-19 01:43:05 +0800

    tmp bump runtime version

--- runtime/khala/src/lib.rs
+++ runtime/khala/src/lib.rs
@@ -121,10 +121,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("khala"),
     impl_name: create_runtime_str!("khala"),
     authoring_version: 1,
-    spec_version: 12,
+    spec_version: 13,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
-    transaction_version: 1,
+    transaction_version: 2,
 };
 
 /// The version information used to identify this runtime when compiled natively.
