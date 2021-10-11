848a78ecb044af9135beaa0ab830867d89aed444
jasl <jasl9187@hotmail.com>
2021-10-11 18:57:26 +0800

    wip

--- polkadot/node/service/src/chain_spec.rs
+++ polkadot/node/service/src/chain_spec.rs
@@ -24,8 +24,10 @@ use kusama_runtime as kusama;
 use kusama_runtime::constants::currency::UNITS as KSM;
 use pallet_im_online::sr25519::AuthorityId as ImOnlineId;
 use pallet_staking::Forcing;
+#[cfg(feature = "polkadot-native")]
 use polkadot::constants::currency::UNITS as DOT;
 use polkadot_primitives::v1::{AccountId, AccountPublic, AssignmentId, ValidatorId};
+#[cfg(feature = "polkadot-native")]
 use polkadot_runtime as polkadot;
 use sp_authority_discovery::AuthorityId as AuthorityDiscoveryId;
 use sp_consensus_babe::AuthorityId as BabeId;
@@ -44,6 +46,7 @@ use westend_runtime as westend;
 #[cfg(feature = "westend-native")]
 use westend_runtime::constants::currency::UNITS as WND;
 
+#[cfg(feature = "polkadot-native")]
 const POLKADOT_STAGING_TELEMETRY_URL: &str = "wss://telemetry.polkadot.io/submit/";
 #[cfg(feature = "kusama-native")]
 const KUSAMA_STAGING_TELEMETRY_URL: &str = "wss://telemetry.polkadot.io/submit/";
@@ -71,34 +74,42 @@ pub struct Extensions {
 }
 
 /// The `ChainSpec` parameterized for the polkadot runtime.
+#[cfg(feature = "polkadot-native")]
 pub type PolkadotChainSpec = service::GenericChainSpec<polkadot::GenesisConfig, Extensions>;
+
+// Dummy chain spec, in case when we don't have the native runtime.
+pub type DummyChainSpec = service::GenericChainSpec<(), Extensions>;
+
+// Dummy chain spec, but that is fine when we don't have the native runtime.
+#[cfg(not(feature = "polkadot-native"))]
+pub type PolkadotChainSpec = DummyChainSpec;
 
 /// The `ChainSpec` parameterized for the kusama runtime.
 #[cfg(feature = "kusama-native")]
 pub type KusamaChainSpec = service::GenericChainSpec<kusama::GenesisConfig, Extensions>;
 
 /// The `ChainSpec` parameterized for the kusama runtime.
-// This actually uses the polkadot chain spec, but that is fine when we don't have the native runtime.
+// Dummy chain spec, but that is fine when we don't have the native runtime.
 #[cfg(not(feature = "kusama-native"))]
-pub type KusamaChainSpec = PolkadotChainSpec;
+pub type KusamaChainSpec = DummyChainSpec;
 
 /// The `ChainSpec` parameterized for the westend runtime.
 #[cfg(feature = "westend-native")]
 pub type WestendChainSpec = service::GenericChainSpec<westend::GenesisConfig, Extensions>;
 
 /// The `ChainSpec` parameterized for the westend runtime.
-// This actually uses the polkadot chain spec, but that is fine when we don't have the native runtime.
+// Dummy chain spec, but that is fine when we don't have the native runtime.
 #[cfg(not(feature = "westend-native"))]
-pub type WestendChainSpec = PolkadotChainSpec;
+pub type WestendChainSpec = DummyChainSpec;
 
 /// The `ChainSpec` parameterized for the rococo runtime.
 #[cfg(feature = "rococo-native")]
 pub type RococoChainSpec = service::GenericChainSpec<RococoGenesisExt, Extensions>;
 
 /// The `ChainSpec` parameterized for the rococo runtime.
-// This actually uses the polkadot chain spec, but that is fine when we don't have the native runtime.
+// Dummy chain spec, but that is fine when we don't have the native runtime.
 #[cfg(not(feature = "rococo-native"))]
-pub type RococoChainSpec = PolkadotChainSpec;
+pub type RococoChainSpec = DummyChainSpec;
 
 /// Extension for the Rococo genesis config to support a custom changes to the genesis state.
 #[derive(serde::Serialize, serde::Deserialize)]
@@ -146,7 +157,12 @@ pub fn wococo_config() -> Result<RococoChainSpec, String> {
 }
 
 /// The default parachains host configuration.
-#[cfg(any(feature = "rococo-native", feature = "kusama-native", feature = "westend-native"))]
+#[cfg(any(
+	feature = "rococo-native",
+	feature = "kusama-native",
+	feature = "westend-native",
+	feature = "polkadot-native"
+))]
 fn default_parachains_host_configuration(
 ) -> polkadot_runtime_parachains::configuration::HostConfiguration<
 	polkadot_primitives::v1::BlockNumber,
@@ -194,6 +210,7 @@ fn default_parachains_host_configuration(
 	}
 }
 
+#[cfg(feature = "polkadot-native")]
 fn polkadot_session_keys(
 	babe: BabeId,
 	grandpa: GrandpaId,
@@ -271,6 +288,7 @@ fn rococo_session_keys(
 	}
 }
 
+#[cfg(feature = "polkadot-native")]
 fn polkadot_staging_testnet_config_genesis(wasm_binary: &[u8]) -> polkadot::GenesisConfig {
 	// subkey inspect "$SECRET"
 	let endowed_accounts = vec![];
@@ -351,6 +369,10 @@ fn polkadot_staging_testnet_config_genesis(wasm_binary: &[u8]) -> polkadot::Gene
 		claims: polkadot::ClaimsConfig { claims: vec![], vesting: vec![] },
 		vesting: polkadot::VestingConfig { vesting: vec![] },
 		treasury: Default::default(),
+		configuration: polkadot::ConfigurationConfig {
+			config: default_parachains_host_configuration(),
+		},
+		paras: Default::default(),
 	}
 }
 
@@ -533,6 +555,9 @@ fn westend_staging_testnet_config_genesis(wasm_binary: &[u8]) -> westend::Genesi
 			config: default_parachains_host_configuration(),
 		},
 		paras: Default::default(),
+		registrar: westend_runtime::RegistrarConfig {
+			next_free_para_id: polkadot_primitives::v1::LOWEST_PUBLIC_ID,
+		},
 	}
 }
 
@@ -1026,26 +1051,30 @@ fn rococo_staging_testnet_config_genesis(wasm_binary: &[u8]) -> rococo_runtime::
 		configuration: rococo_runtime::ConfigurationConfig {
 			config: default_parachains_host_configuration(),
 		},
-		bridge_rococo_grandpa: rococo_runtime::BridgeRococoGrandpaConfig {
-			owner: Some(endowed_accounts[0].clone()),
-			..Default::default()
+		registrar: rococo_runtime::RegistrarConfig {
+			next_free_para_id: polkadot_primitives::v1::LOWEST_PUBLIC_ID,
 		},
-		bridge_wococo_grandpa: rococo_runtime::BridgeWococoGrandpaConfig {
-			owner: Some(endowed_accounts[0].clone()),
-			..Default::default()
-		},
-		bridge_rococo_messages: rococo_runtime::BridgeRococoMessagesConfig {
-			owner: Some(endowed_accounts[0].clone()),
-			..Default::default()
-		},
-		bridge_wococo_messages: rococo_runtime::BridgeWococoMessagesConfig {
-			owner: Some(endowed_accounts[0].clone()),
-			..Default::default()
-		},
+		// bridge_rococo_grandpa: rococo_runtime::BridgeRococoGrandpaConfig {
+		// 	owner: Some(endowed_accounts[0].clone()),
+		// 	..Default::default()
+		// },
+		// bridge_wococo_grandpa: rococo_runtime::BridgeWococoGrandpaConfig {
+		// 	owner: Some(endowed_accounts[0].clone()),
+		// 	..Default::default()
+		// },
+		// bridge_rococo_messages: rococo_runtime::BridgeRococoMessagesConfig {
+		// 	owner: Some(endowed_accounts[0].clone()),
+		// 	..Default::default()
+		// },
+		// bridge_wococo_messages: rococo_runtime::BridgeWococoMessagesConfig {
+		// 	owner: Some(endowed_accounts[0].clone()),
+		// 	..Default::default()
+		// },
 	}
 }
 
 /// Polkadot staging testnet config.
+#[cfg(feature = "polkadot-native")]
 pub fn polkadot_staging_testnet_config() -> Result<PolkadotChainSpec, String> {
 	let wasm_binary = polkadot::WASM_BINARY.ok_or("Polkadot development wasm not available")?;
 	let boot_nodes = vec![];
@@ -1211,6 +1240,7 @@ fn testnet_accounts() -> Vec<AccountId> {
 }
 
 /// Helper function to create polkadot `GenesisConfig` for testing
+#[cfg(feature = "polkadot-native")]
 pub fn polkadot_testnet_genesis(
 	wasm_binary: &[u8],
 	initial_authorities: Vec<(
@@ -1289,6 +1319,10 @@ pub fn polkadot_testnet_genesis(
 		claims: polkadot::ClaimsConfig { claims: vec![], vesting: vec![] },
 		vesting: polkadot::VestingConfig { vesting: vec![] },
 		treasury: Default::default(),
+		configuration: polkadot::ConfigurationConfig {
+			config: default_parachains_host_configuration(),
+		},
+		paras: Default::default(),
 	}
 }
 
@@ -1455,6 +1489,9 @@ pub fn westend_testnet_genesis(
 			config: default_parachains_host_configuration(),
 		},
 		paras: Default::default(),
+		registrar: westend_runtime::RegistrarConfig {
+			next_free_para_id: polkadot_primitives::v1::LOWEST_PUBLIC_ID,
+		},
 	}
 }
 
@@ -1525,25 +1562,29 @@ pub fn rococo_testnet_genesis(
 		},
 		hrmp: Default::default(),
 		paras: rococo_runtime::ParasConfig { paras: vec![] },
-		bridge_rococo_grandpa: rococo_runtime::BridgeRococoGrandpaConfig {
-			owner: Some(root_key.clone()),
-			..Default::default()
+		registrar: rococo_runtime::RegistrarConfig {
+			next_free_para_id: polkadot_primitives::v1::LOWEST_PUBLIC_ID,
 		},
-		bridge_wococo_grandpa: rococo_runtime::BridgeWococoGrandpaConfig {
-			owner: Some(root_key.clone()),
-			..Default::default()
-		},
-		bridge_rococo_messages: rococo_runtime::BridgeRococoMessagesConfig {
-			owner: Some(root_key.clone()),
-			..Default::default()
-		},
-		bridge_wococo_messages: rococo_runtime::BridgeWococoMessagesConfig {
-			owner: Some(root_key.clone()),
-			..Default::default()
-		},
+		// bridge_rococo_grandpa: rococo_runtime::BridgeRococoGrandpaConfig {
+		// 	owner: Some(root_key.clone()),
+		// 	..Default::default()
+		// },
+		// bridge_wococo_grandpa: rococo_runtime::BridgeWococoGrandpaConfig {
+		// 	owner: Some(root_key.clone()),
+		// 	..Default::default()
+		// },
+		// bridge_rococo_messages: rococo_runtime::BridgeRococoMessagesConfig {
+		// 	owner: Some(root_key.clone()),
+		// 	..Default::default()
+		// },
+		// bridge_wococo_messages: rococo_runtime::BridgeWococoMessagesConfig {
+		// 	owner: Some(root_key.clone()),
+		// 	..Default::default()
+		// },
 	}
 }
 
+#[cfg(feature = "polkadot-native")]
 fn polkadot_development_config_genesis(wasm_binary: &[u8]) -> polkadot::GenesisConfig {
 	polkadot_testnet_genesis(
 		wasm_binary,
@@ -1584,6 +1625,7 @@ fn rococo_development_config_genesis(wasm_binary: &[u8]) -> rococo_runtime::Gene
 }
 
 /// Polkadot development config (single validator Alice)
+#[cfg(feature = "polkadot-native")]
 pub fn polkadot_development_config() -> Result<PolkadotChainSpec, String> {
 	let wasm_binary = polkadot::WASM_BINARY.ok_or("Polkadot development wasm not available")?;
 
@@ -1681,6 +1723,7 @@ pub fn wococo_development_config() -> Result<RococoChainSpec, String> {
 	))
 }
 
+#[cfg(feature = "polkadot-native")]
 fn polkadot_local_testnet_genesis(wasm_binary: &[u8]) -> polkadot::GenesisConfig {
 	polkadot_testnet_genesis(
 		wasm_binary,
@@ -1694,6 +1737,7 @@ fn polkadot_local_testnet_genesis(wasm_binary: &[u8]) -> polkadot::GenesisConfig
 }
 
 /// Polkadot local testnet config (multivalidator Alice + Bob)
+#[cfg(feature = "polkadot-native")]
 pub fn polkadot_local_testnet_config() -> Result<PolkadotChainSpec, String> {
 	let wasm_binary = polkadot::WASM_BINARY.ok_or("Polkadot development wasm not available")?;
 
--- runtime/khala/src/lib.rs
+++ runtime/khala/src/lib.rs
@@ -256,9 +256,9 @@ impl Contains<Call> for BaseCallFilter {
             Call::Authorship(_) | Call::CollatorSelection(_) | Call::Session(_) |
             // Governance
             Call::Identity(_) | Call::Treasury(_) |
-            Call::Democracy(_) | Call::PhragmenElection(..) |
+            Call::Democracy(_) | Call::PhragmenElection { .. } |
             Call::Council(_) | Call::TechnicalCommittee(_) | Call::TechnicalMembership(_) |
-            Call::Bounties(_) | Call::Lottery(_) | Call::Tips(..) |
+            Call::Bounties(_) | Call::Lottery(_) | Call::Tips { .. } |
             // Phala
             Call::PhalaMq(_) | Call::PhalaRegistry(_) |
             Call::PhalaMining(_) | Call::PhalaStakePool(_)
@@ -370,7 +370,10 @@ parameter_types! {
 }
 
 /// The type used to represent the kinds of proxying allowed.
-#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Encode, Decode, RuntimeDebug, MaxEncodedLen)]
+#[derive(
+    Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Encode, Decode, RuntimeDebug, MaxEncodedLen,
+    scale_info::TypeInfo,
+)]
 pub enum ProxyType {
     /// Fully permissioned proxy. Can execute any call on behalf of _proxied_.
     Any,
@@ -397,59 +400,59 @@ impl InstanceFilter<Call> for ProxyType {
             ProxyType::Any => true,
             ProxyType::NonTransfer => matches!(
                 c,
-                Call::System(..) |
-                Call::Timestamp(..) |
-                Call::Session(..) |
-                Call::Democracy(..) |
-                Call::Council(..) |
-                Call::PhragmenElection(..) |
-                Call::TechnicalCommittee(..) |
-                Call::TechnicalMembership(..) |
-                Call::Treasury(..) |
-                Call::Bounties(..) |
-                Call::Tips(..) |
-                Call::Utility(..) |
-                Call::Identity(..) |
-                Call::Vesting(pallet_vesting::Call::vest(..)) |
-                Call::Vesting(pallet_vesting::Call::vest_other(..)) |
-                Call::Scheduler(..) |
-                Call::Proxy(..) |
-                Call::Multisig(..)
+                Call::System { .. } |
+                Call::Timestamp { .. } |
+                Call::Session { .. } |
+                Call::Democracy { .. } |
+                Call::Council { .. } |
+                Call::PhragmenElection { .. } |
+                Call::TechnicalCommittee { .. } |
+                Call::TechnicalMembership { .. } |
+                Call::Treasury { .. } |
+                Call::Bounties { .. } |
+                Call::Tips { .. } |
+                Call::Utility { .. } |
+                Call::Identity { .. } |
+                Call::Vesting(pallet_vesting::Call::vest { .. }) |
+                Call::Vesting(pallet_vesting::Call::vest_other { .. }) |
+                Call::Scheduler { .. } |
+                Call::Proxy { .. } |
+                Call::Multisig { .. }
             ),
             ProxyType::CancelProxy => matches!(
                 c,
-                Call::Proxy(pallet_proxy::Call::reject_announcement(..)) |
-                Call::Utility(..) |
-                Call::Multisig(..)
+                Call::Proxy(pallet_proxy::Call::reject_announcement { .. }) |
+                Call::Utility { .. } |
+                Call::Multisig { .. }
             ),
             ProxyType::Governance => matches!(
                 c,
-                Call::Democracy(..) |
-                Call::PhragmenElection(..) |
-                Call::Council(..) |
-                Call::TechnicalCommittee(..) |
-                Call::Treasury(..) |
-                Call::Utility(..) |
-                Call::Bounties(..) |
-                Call::Tips(..) |
-                Call::Lottery(..)
+                Call::Democracy { .. } |
+                Call::PhragmenElection { .. } |
+                Call::Council { .. } |
+                Call::TechnicalCommittee { .. } |
+                Call::Treasury { .. } |
+                Call::Utility { .. } |
+                Call::Bounties { .. } |
+                Call::Tips { .. } |
+                Call::Lottery { .. }
             ),
             ProxyType::Collator => matches!(
                 c,
-                Call::CollatorSelection(..) |
-                Call::Utility(..) |
-                Call::Multisig(..)
+                Call::CollatorSelection { .. } |
+                Call::Utility { .. } |
+                Call::Multisig { .. }
             ),
             ProxyType::StakePoolManager => matches!(
                 c,
-                Call::PhalaStakePool(pallet_stakepool::Call::add_worker(..)) |
-                Call::PhalaStakePool(pallet_stakepool::Call::remove_worker(..)) |
-                Call::PhalaStakePool(pallet_stakepool::Call::start_mining(..)) |
-                Call::PhalaStakePool(pallet_stakepool::Call::stop_mining(..)) |
-                Call::PhalaStakePool(pallet_stakepool::Call::reclaim_pool_worker(..)) |
-                Call::PhalaStakePool(pallet_stakepool::Call::create(..)) |
-                Call::PhalaRegistry(pallet_registry::Call::register_worker(..)) |
-                Call::PhalaMq(pallet_mq::Call::sync_offchain_message(..))
+                Call::PhalaStakePool(pallet_stakepool::Call::add_worker { .. }) |
+                Call::PhalaStakePool(pallet_stakepool::Call::remove_worker { .. }) |
+                Call::PhalaStakePool(pallet_stakepool::Call::start_mining { .. }) |
+                Call::PhalaStakePool(pallet_stakepool::Call::stop_mining { .. }) |
+                Call::PhalaStakePool(pallet_stakepool::Call::reclaim_pool_worker { .. }) |
+                Call::PhalaStakePool(pallet_stakepool::Call::create { .. }) |
+                Call::PhalaRegistry(pallet_registry::Call::register_worker { .. }) |
+                Call::PhalaMq(pallet_mq::Call::sync_offchain_message { .. })
             ),
         }
     }
@@ -531,6 +534,7 @@ impl pallet_balances::Config for Runtime {
 parameter_types! {
     pub const TransactionByteFee: Balance = 1 * MILLICENTS;
     pub const TargetBlockFullness: Perquintill = Perquintill::from_percent(25);
+    pub const OperationalFeeMultiplier: u8 = 5;
     pub AdjustmentVariable: pallet_transaction_payment::Multiplier =
         pallet_transaction_payment::Multiplier::saturating_from_rational(1, 100_000);
     pub MinimumMultiplier: pallet_transaction_payment::Multiplier =
@@ -561,6 +565,7 @@ impl pallet_transaction_payment::Config for Runtime {
         AdjustmentVariable,
         MinimumMultiplier,
     >;
+    type OperationalFeeMultiplier = OperationalFeeMultiplier;
 }
 
 impl pallet_bounties::Config for Runtime {
@@ -1061,7 +1066,7 @@ impl_runtime_apis! {
 
     impl sp_api::Metadata<Block> for Runtime {
         fn metadata() -> OpaqueMetadata {
-            Runtime::metadata().into()
+            OpaqueMetadata::new(Runtime::metadata().into())
         }
     }
 
