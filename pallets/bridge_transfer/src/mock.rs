25e4dcf6fe61167a89f78d02a9dbe012b14a1071
jasl <jasl9187@hotmail.com>
2021-10-14 00:24:06 +0800

    Integrate phala-blockchain changes til 4c54ce059fef69ffa87332f81c3523d0a6c7768d

--- pallets/bridge_transfer/src/mock.rs
+++ pallets/bridge_transfer/src/mock.rs
@@ -1,5 +1,7 @@
 #![cfg(test)]
 
+use super::*;
+
 use frame_support::{ord_parameter_types, parameter_types, weights::Weight, PalletId};
 use frame_system::{self as system};
 use hex_literal::hex;
@@ -13,6 +15,7 @@ use sp_runtime::{
 use crate::{self as bridge_transfer, Config};
 pub use pallet_balances as balances;
 use pallet_bridge as bridge;
+use phala_pallets::{pallet_mq as mq, pallet_registry as reg};
 
 type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
 type Block = frame_system::mocking::MockBlock<Test>;
@@ -27,6 +30,8 @@ frame_support::construct_runtime!(
 		Balances: pallet_balances::{Pallet, Call, Storage, Config<T>, Event<T>},
 		Bridge: bridge::{Pallet, Call, Storage, Event<T>},
 		BridgeTransfer: bridge_transfer::{Pallet, Call, Storage, Event<T>},
+		PhalaMq: mq::{Pallet, Call, Storage},
+		PhalaRegistry: reg::{Pallet, Call, Event, Storage},
 		Timestamp: pallet_timestamp::{Pallet, Call, Storage, Inherent},
 	}
 );
@@ -101,15 +106,44 @@ impl bridge::Config for Test {
 
 parameter_types! {
 	// bridge::derive_resource_id(1, &bridge::hashing::blake2_128(b"PHA"));
-	pub const BridgeTokenId: [u8; 32] = hex!("00000000000000000000000000000063a7e2be78898ba83824b0c0cc8dfb6001");
+	pub const NativeTokenResourceId: [u8; 32] = hex!("00000000000000000000000000000063a7e2be78898ba83824b0c0cc8dfb6001");
 }
 
 impl Config for Test {
 	type Event = Event;
 	type BridgeOrigin = bridge::EnsureBridge<Test>;
 	type Currency = Balances;
-	type BridgeTokenId = BridgeTokenId;
+	type NativeTokenResourceId = NativeTokenResourceId;
 	type OnFeePay = ();
+}
+
+impl mq::Config for Test {
+	type CallMatcher = MqCallMatcher;
+	type QueueNotifyConfig = ();
+}
+
+pub struct MqCallMatcher;
+impl mq::CallMatcher<Test> for MqCallMatcher {
+	fn match_call(call: &Call) -> Option<&mq::Call<Test>> {
+		match call {
+			Call::PhalaMq(mq_call) => Some(mq_call),
+			_ => None,
+		}
+	}
+}
+
+parameter_types! {
+	pub const VerifyPRuntime: bool = false;
+	pub const VerifyRelaychainGenesisBlockHash: bool = false;
+}
+
+impl reg::Config for Test {
+	type Event = Event;
+	type AttestationValidator = reg::IasValidator;
+	type UnixTime = Timestamp;
+	type VerifyPRuntime = VerifyPRuntime;
+	type VerifyRelaychainGenesisBlockHash = VerifyRelaychainGenesisBlockHash;
+	type GovernanceOrigin = frame_system::EnsureRoot<Self::AccountId>;
 }
 
 impl pallet_timestamp::Config for Test {
