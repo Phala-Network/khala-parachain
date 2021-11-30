02f4a7060a54f0cac32984d855e110d8efcdc3eb
jasl <jasl9187@hotmail.com>
2021-12-01 05:52:55 +0800

    remove some warnings

--- runtime/khala/src/lib.rs
+++ runtime/khala/src/lib.rs
@@ -52,7 +52,7 @@ use sp_core::{
 };
 use sp_runtime::{
     create_runtime_str, generic, impl_opaque_keys,
-    traits::{AccountIdLookup, Block as BlockT, ConvertInto, Zero},
+    traits::{AccountIdLookup, Block as BlockT, ConvertInto},
     transaction_validity::{TransactionSource, TransactionValidity},
     ApplyExtrinsicResult, FixedPointNumber, Perbill, Percent, Permill, Perquintill,
 };
