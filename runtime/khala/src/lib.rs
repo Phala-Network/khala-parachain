922ea2d23eb6391d0e1b69e48617131a7930514c
jasl <jasl9187@hotmail.com>
2021-12-10 13:07:51 +0800

    Bump

--- runtime/khala/src/lib.rs
+++ runtime/khala/src/lib.rs
@@ -123,7 +123,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("khala"),
     impl_name: create_runtime_str!("khala"),
     authoring_version: 1,
-    spec_version: 1080,
+    spec_version: 1090,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 2,
