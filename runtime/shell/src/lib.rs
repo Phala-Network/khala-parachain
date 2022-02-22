bcddbbc66fec43684b8d71f27b962687600c1d34
jasl <jasl9187@hotmail.com>
2022-02-22 15:19:35 +0800

    Set spec_version to 1 for Shell runtime

--- runtime/shell/src/lib.rs
+++ runtime/shell/src/lib.rs
@@ -67,7 +67,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("khala"),
 	impl_name: create_runtime_str!("shell"),
 	authoring_version: 1,
-	spec_version: 2,
+	spec_version: 1,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
