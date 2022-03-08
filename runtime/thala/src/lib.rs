10bf7908d470aec7f1e0d90a25bee1f4033c6fe1
jasl <jasl9187@hotmail.com>
2022-03-08 18:25:50 +0800

    Bump to 0.1.12-dev

--- runtime/thala/src/lib.rs
+++ runtime/thala/src/lib.rs
@@ -147,7 +147,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("thala"),
     impl_name: create_runtime_str!("thala"),
     authoring_version: 1,
-    spec_version: 1110,
+    spec_version: 1119,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 3,
