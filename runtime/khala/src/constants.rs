4ec82702e5bccaa984ab4143e297af601c59390a
Wenfeng Wang <kalot.wang@gmail.com>
2022-03-09 12:15:44 +0800

    add kusd

--- runtime/khala/src/constants.rs
+++ runtime/khala/src/constants.rs
@@ -83,6 +83,7 @@ pub mod parachains {
     pub mod karura {
         pub const ID: u32 = 2000;
         pub const KAR_KEY: [u8; 2] = [0, 128];
+        pub const KUSD_KEY: [u8; 2] = [0, 129];
     }
 
     pub mod bifrost {
