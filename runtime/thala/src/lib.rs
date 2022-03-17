9c31da8393f876d8feca2c074b63b6da04ea70ff
Wenfeng Wang <kalot.wang@gmail.com>
2022-03-17 03:58:27 +0000

    enable call for  assets-wrapper

--- runtime/thala/src/lib.rs
+++ runtime/thala/src/lib.rs
@@ -323,6 +323,7 @@ impl Contains<Call> for BaseCallFilter {
             // Parachain
             Call::ParachainSystem { .. } |
             // Monetary
+            Call::AssetsWrapper { .. } |
             Call::Balances { .. }  |
             Call::ChainBridge { .. } |
             Call::BridgeTransfer { .. } |
