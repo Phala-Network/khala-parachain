f3d6521002d7119270687718fab83e69f4edbe0b
Wenfeng Wang <kalot.wang@gmail.com>
2022-03-23 20:20:01 +0800

    remove unused lines

--- pallets/xtransfer/src/lib.rs
+++ pallets/xtransfer/src/lib.rs
@@ -7,9 +7,3 @@ mod mock;
 pub mod traits;
 pub mod xcm_transfer;
 pub mod xtransfer;
-
-// pub use chainbridge::*;
-// pub use fungible_adapter::*;
-// pub use traits::*;
-// pub use xcm_transfer::*;
-// pub use xtransfer::*;
