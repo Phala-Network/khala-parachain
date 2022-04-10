dfb2c2c6388aaca778a15368020f52662982841c
jasl <jasl9187@hotmail.com>
2022-04-11 06:16:52 +0800

    Remove stale migrations

--- runtime/khala/src/migrations.rs
+++ runtime/khala/src/migrations.rs
@@ -2,20 +2,3 @@
 use super::*;
 #[allow(unused_imports)]
 use frame_support::traits::OnRuntimeUpgrade;
-pub struct PhalaV4Migration;
-
-impl OnRuntimeUpgrade for PhalaV4Migration {
-	fn on_runtime_upgrade() -> frame_support::weights::Weight {
-		phala_pallets::migrations::v4::migrate::<Runtime>()
-	}
-
-	#[cfg(feature = "try-runtime")]
-	fn pre_upgrade() -> Result<(), &'static str> {
-		phala_pallets::migrations::v4::pre_migrate::<Runtime>()
-	}
-
-	#[cfg(feature = "try-runtime")]
-	fn post_upgrade() -> Result<(), &'static str> {
-		phala_pallets::migrations::v4::post_migrate::<Runtime>()
-	}
-}
