ce6618769bf8aa3634a7e522a57a1fec3ea95852
h4x3rotab <h4x3rotab@gmail.com>
2022-04-21 10:29:21 +0000

    phala: integrate pallet v5 (unresp-fix)

--- runtime/khala/src/migrations.rs
+++ runtime/khala/src/migrations.rs
@@ -37,3 +37,21 @@ impl OnRuntimeUpgrade for AssetsRegistryMigrations {
         assets_registry::migration::assets_registry_migration::post_migrate::<Runtime>()
     }
 }
+
+pub struct PhalaPalletsV5;
+
+impl OnRuntimeUpgrade for PhalaPalletsV5 {
+    fn on_runtime_upgrade() -> frame_support::weights::Weight {
+        phala_pallets::migrations::v5::migrate::<Runtime>()
+    }
+
+    #[cfg(feature = "try-runtime")]
+    fn pre_upgrade() -> Result<(), &'static str> {
+        phala_pallets::migrations::v5::pre_migrate::<Runtime>()
+    }
+
+    #[cfg(feature = "try-runtime")]
+    fn post_upgrade() -> Result<(), &'static str> {
+        phala_pallets::migrations::v5::post_migrate::<Runtime>()
+    }
+}
