5848576643acde41886eb41f08730a482dbb51f4
jasl <jasl9187@hotmail.com>
2022-05-06 23:25:36 +0800

    Upgrade to Polkadot 0.9.20

--- node/src/command.rs
+++ node/src/command.rs
@@ -582,6 +582,7 @@ pub fn run() -> Result<()> {
                     })
                 }),
                 BenchmarkCmd::Overhead(_) => Err("Unsupported benchmarking command".into()),
+                BenchmarkCmd::Machine(cmd) => runner.sync_run(|config| cmd.run(&config)),
             }
         },
         Some(Subcommand::Key(cmd)) => Ok(cmd.run(&cli)?),
