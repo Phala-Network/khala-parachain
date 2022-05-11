dddaad25e3611c2bf8a21335ecd2b28d18918703
h4x3rotab <h4x3rotab@gmail.com>
2022-05-11 12:56:25 +0000

    phala: migration v6

--- runtime/khala/src/lib.rs
+++ runtime/khala/src/lib.rs
@@ -190,6 +190,7 @@ pub type Executive = frame_executive::Executive<
     (
         migrations::SubbridgeMigrations,
         migrations::AssetsRegistryMigrations,
+        migrations::PhalaPalletV6Migrations,
     ),
 >;
 
