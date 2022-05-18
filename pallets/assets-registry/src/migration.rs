935f7446f0f80639a65a6bbf27f5936ebca9921e
Wenfeng Wang <kalot.wang@gmail.com>
2022-05-18 15:32:37 +0800

    fix migration

--- pallets/assets-registry/src/migration.rs
+++ pallets/assets-registry/src/migration.rs
@@ -2,53 +2,68 @@ use super::*;
 
 mod assets_registry_v3_migration_common {
 	use super::*;
-	use frame_support::{ensure, traits::StorageVersion};
+	use codec::{Decode, Encode};
+	use frame_support::traits::StorageVersion;
+	use scale_info::TypeInfo;
+	use sp_std::vec::Vec;
+	use xcm::latest::MultiLocation;
+
+	#[derive(Clone, Decode, Encode, Eq, PartialEq, Ord, PartialOrd, Debug, TypeInfo)]
+	pub struct OldAssetRegistryInfo {
+		pub location: MultiLocation,
+		pub reserve_location: Option<MultiLocation>,
+		pub enabled_bridges: Vec<XBridge>,
+		pub properties: AssetProperties,
+	}
 
 	pub const EXPECTED_STORAGE_VERSION: StorageVersion = StorageVersion::new(1);
 	pub const FINAL_STORAGE_VERSION: StorageVersion = StorageVersion::new(2);
-	pub const KSM_PRICE: u128 = 111;
-	pub const KAR_PRICE: u128 = 111;
-	pub const BNC_PRICE: u128 = 111;
-	pub const ZLK_PRICE: u128 = 111;
-	pub const AUSD_PRICE: u128 = 111;
-	pub const BSX_PRICE: u128 = 111;
+	pub const PHA_PER_SECOND: u128 = 8_000_000_000;
+	pub const KSM_PRICE: u128 = PHA_PER_SECOND / 600;
+	pub const KAR_PRICE: u128 = PHA_PER_SECOND / 8;
+	pub const BNC_PRICE: u128 = PHA_PER_SECOND / 4;
+	pub const ZLK_PRICE: u128 = (PHA_PER_SECOND / 4) * 1_000_000; // decimals 18
+	pub const AUSD_PRICE: u128 = PHA_PER_SECOND / 8;
+	pub const BSX_PRICE: u128 = PHA_PER_SECOND;
+	pub const MOVR_PRICE: u128 = (PHA_PER_SECOND / 240) * 1_000_000; // decimals 18
+	pub const HKO_PRICE: u128 = PHA_PER_SECOND;
+
+	pub fn lookup_price<T: Config>(
+		assets: &[(<T as pallet_assets::Config>::AssetId, u128)],
+		asset_id: <T as pallet_assets::Config>::AssetId,
+	) -> Option<u128> {
+		for (id, price) in assets.iter() {
+			if *id == asset_id {
+				return Some(*price);
+			}
+		}
+		None
+	}
 
 	pub fn migrate_asset_price<T: Config>(
 		assets: &[(<T as pallet_assets::Config>::AssetId, u128)],
 	) -> frame_support::weights::Weight {
-		for (asset_id, execution_price) in assets.iter() {
-			let mut reg_info = RegistryInfoByIds::<T>::get(asset_id).unwrap();
-
-			reg_info.execution_price = Some(*execution_price);
-			RegistryInfoByIds::<T>::insert(asset_id, &reg_info);
-		}
+		RegistryInfoByIds::<T>::translate(
+			|asset_id: <T as pallet_assets::Config>::AssetId, old_info: OldAssetRegistryInfo| {
+				if let Some(execution_price) = lookup_price::<T>(assets, asset_id) {
+					Some(AssetRegistryInfo {
+						location: old_info.location,
+						reserve_location: old_info.reserve_location,
+						enabled_bridges: old_info.enabled_bridges,
+						properties: old_info.properties,
+						execution_price: Some(execution_price),
+					})
+				} else {
+					log::error!(
+						"Asset register info not found: ${:?}, would be deleted",
+						asset_id
+					);
+					None
+				}
+			},
+		);
 
 		assets.len() as u64
-	}
-
-	pub fn pre_check_price<T: Config>(
-		ids: &[<T as pallet_assets::Config>::AssetId],
-	) -> Result<(), &'static str> {
-		for asset_id in ids.iter() {
-			let reg_info =
-				RegistryInfoByIds::<T>::get(asset_id).ok_or(Error::<T>::AssetNotRegistered)?;
-			ensure!(reg_info.execution_price == None, "Incorrect registry info");
-		}
-		Ok(())
-	}
-
-	pub fn post_check_price<T: Config>(
-		assets: &[(<T as pallet_assets::Config>::AssetId, u128)],
-	) -> Result<(), &'static str> {
-		for (asset_id, price) in assets {
-			let reg_info =
-				RegistryInfoByIds::<T>::get(asset_id).ok_or(Error::<T>::AssetNotRegistered)?;
-			ensure!(
-				reg_info.execution_price == Some(*price),
-				"Incorrect registry info of KSM"
-			);
-		}
-		Ok(())
 	}
 }
 
@@ -70,16 +85,6 @@ pub mod assets_registry_v3_migration_for_rhala {
 			StorageVersion::get::<Pallet<T>>() == common::EXPECTED_STORAGE_VERSION,
 			"Incorrect AssetRegistry storage version in pre migrate"
 		);
-
-		let roc_id: <T as pallet_assets::Config>::AssetId = 0u32.into();
-		let kar_id: <T as pallet_assets::Config>::AssetId = 1u32.into();
-		let bnc_id: <T as pallet_assets::Config>::AssetId = 2u32.into();
-		let zlk_id: <T as pallet_assets::Config>::AssetId = 3u32.into();
-		let ausd_id: <T as pallet_assets::Config>::AssetId = 4u32.into();
-		let bsx_id: <T as pallet_assets::Config>::AssetId = 5u32.into();
-
-		common::pre_check_price::<T>(&[roc_id, kar_id, bnc_id, zlk_id, ausd_id, bsx_id])?;
-
 		log::info!("Assets registry pre migration check passed👏");
 
 		Ok(())
@@ -130,23 +135,6 @@ pub mod assets_registry_v3_migration_for_rhala {
 			StorageVersion::get::<Pallet<T>>() == common::FINAL_STORAGE_VERSION,
 			"Incorrect AssetRegistry storage version in post migrate"
 		);
-
-		let roc_id: <T as pallet_assets::Config>::AssetId = 0u32.into();
-		let kar_id: <T as pallet_assets::Config>::AssetId = 1u32.into();
-		let bnc_id: <T as pallet_assets::Config>::AssetId = 2u32.into();
-		let zlk_id: <T as pallet_assets::Config>::AssetId = 3u32.into();
-		let ausd_id: <T as pallet_assets::Config>::AssetId = 4u32.into();
-		let bsx_id: <T as pallet_assets::Config>::AssetId = 5u32.into();
-
-		common::post_check_price::<T>(&[
-			(roc_id, common::KSM_PRICE),
-			(kar_id, common::KAR_PRICE),
-			(bnc_id, common::BNC_PRICE),
-			(zlk_id, common::ZLK_PRICE),
-			(ausd_id, common::AUSD_PRICE),
-			(bsx_id, common::BSX_PRICE),
-		])?;
-
 		log::info!("Assets registry post migration check passed👏");
 
 		Ok(())
@@ -167,19 +155,10 @@ pub mod assets_registry_v3_migration_for_khala {
 		T: Config,
 		<T as pallet_assets::Config>::AssetId: From<u32>,
 	{
-		let ksm_id: <T as pallet_assets::Config>::AssetId = 0u32.into();
-		let kar_id: <T as pallet_assets::Config>::AssetId = 1u32.into();
-		let bnc_id: <T as pallet_assets::Config>::AssetId = 2u32.into();
-		let zlk_id: <T as pallet_assets::Config>::AssetId = 3u32.into();
-		let ausd_id: <T as pallet_assets::Config>::AssetId = 4u32.into();
-
-		common::pre_check_price::<T>(&[ksm_id, kar_id, bnc_id, zlk_id, ausd_id])?;
-
 		ensure!(
 			StorageVersion::get::<Pallet<T>>() == common::EXPECTED_STORAGE_VERSION,
 			"Incorrect AssetRegistry storage version in pre migrate"
 		);
-
 		log::info!("Assets registry pre migration check passed👏");
 
 		Ok(())
@@ -198,6 +177,9 @@ pub mod assets_registry_v3_migration_for_khala {
 			let bnc_id: <T as pallet_assets::Config>::AssetId = 2u32.into();
 			let zlk_id: <T as pallet_assets::Config>::AssetId = 3u32.into();
 			let ausd_id: <T as pallet_assets::Config>::AssetId = 4u32.into();
+			let bsx_id: <T as pallet_assets::Config>::AssetId = 5u32.into();
+			let movr_id: <T as pallet_assets::Config>::AssetId = 6u32.into();
+			let hko_id: <T as pallet_assets::Config>::AssetId = 7u32.into();
 
 			// Update execution prices
 			write_count += common::migrate_asset_price::<T>(&[
@@ -206,6 +188,9 @@ pub mod assets_registry_v3_migration_for_khala {
 				(bnc_id, common::BNC_PRICE),
 				(zlk_id, common::ZLK_PRICE),
 				(ausd_id, common::AUSD_PRICE),
+				(bsx_id, common::BSX_PRICE),
+				(movr_id, common::MOVR_PRICE),
+				(hko_id, common::HKO_PRICE),
 			]);
 
 			// Set new storage version
@@ -224,25 +209,10 @@ pub mod assets_registry_v3_migration_for_khala {
 		T: Config,
 		<T as pallet_assets::Config>::AssetId: From<u32>,
 	{
-		let ksm_id: <T as pallet_assets::Config>::AssetId = 0u32.into();
-		let kar_id: <T as pallet_assets::Config>::AssetId = 1u32.into();
-		let bnc_id: <T as pallet_assets::Config>::AssetId = 2u32.into();
-		let zlk_id: <T as pallet_assets::Config>::AssetId = 3u32.into();
-		let ausd_id: <T as pallet_assets::Config>::AssetId = 4u32.into();
-
 		ensure!(
 			StorageVersion::get::<Pallet<T>>() == common::FINAL_STORAGE_VERSION,
 			"Incorrect AssetRegistry storage version in post migrate"
 		);
-
-		common::post_check_price::<T>(&[
-			(ksm_id, common::KSM_PRICE),
-			(kar_id, common::KAR_PRICE),
-			(bnc_id, common::BNC_PRICE),
-			(zlk_id, common::ZLK_PRICE),
-			(ausd_id, common::AUSD_PRICE),
-		])?;
-
 		log::info!("Assets registry post migration check passed👏");
 
 		Ok(())
