3b92bfc7f9179579e9935555125b2932e1f374d5
jasl <jasl9187@hotmail.com>
2022-06-20 22:10:08 +0800

    Phala runtime: remove sudo

--- runtime/phala/src/lib.rs
+++ runtime/phala/src/lib.rs
@@ -251,8 +251,8 @@ construct_runtime! {
         XTransfer: xtransfer::{Pallet, Call, Storage, Event<T>} = 82,
         AssetsRegistry: assets_registry::{Pallet, Call, Storage, Event<T>} = 83,
 
-        // TODO: Remove sudo in the future
-        Sudo: pallet_sudo::{Pallet, Call, Storage, Config<T>, Event<T>} = 255,
+        // `sudo` has been removed on production
+        // Sudo: pallet_sudo::{Pallet, Call, Storage, Config<T>, Event<T>} = 255,
     }
 }
 
@@ -288,8 +288,6 @@ impl Contains<Call> for BaseCallFilter {
 
         matches!(
             call,
-            // Sudo
-            Call::Sudo { .. } |
             // System
             Call::System { .. } | Call::Timestamp { .. } | Call::Utility { .. } |
             Call::Multisig { .. } | Call::Proxy { .. } | Call::Scheduler { .. } |
@@ -511,11 +509,6 @@ impl InstanceFilter<Call> for ProxyType {
             _ => false,
         }
     }
-}
-
-impl pallet_sudo::Config for Runtime {
-    type Call = Call;
-    type Event = Event;
 }
 
 impl pallet_proxy::Config for Runtime {
