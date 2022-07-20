1cade32288a4b902cad7cc9adac7b2f804224dd6
Wenfeng Wang <kalot.wang@gmail.com>
2022-07-20 15:32:38 +0800

    remove unused primitive

--- pallets/subbridge/src/xtransfer.rs
+++ pallets/subbridge/src/xtransfer.rs
@@ -17,8 +17,6 @@ pub mod pallet {
 	#[pallet::pallet]
 	#[pallet::generate_store(pub(super) trait Store)]
 	#[pallet::storage_version(STORAGE_VERSION)]
-	// TODO: remove when we Vec get replaced by BoundedVec
-	#[pallet::without_storage_info]
 	pub struct Pallet<T>(_);
 
 	#[pallet::config]
