b1d01c7c406be9e2bc8e3ee056ba3a03f8907abc
hashwarlock <hashwarlock@protonmail.com>
2022-10-07 18:26:29 -0500

    Add todo for fetch queries

---
+++ scripts/js/pw/util/fetch.js
@@ -0,0 +1,53 @@
+require('dotenv').config();
+
+// export async function getBalance(khalaApi, account)
+
+// export async function getOverlord(khalaApi)
+
+// export async function getZeroDay(khalaApi)
+
+// export async function getEra(khalaApi)
+
+// export async function getClaimSpiritStatus(khalaApi)
+
+// export async function getPurchaseRareOriginOfShellsStatus(khalaApi)
+
+// export async function getPurchasePrimeOriginOfShellsStatus(khalaApi)
+
+// export async function getPreorderOriginOfShellsStatus(khalaApi)
+
+// export async function getLastDayOfSaleStatus(khalaApi)
+
+// export async function getSpiritCollectionId(khalaApi)
+
+// export async function getOriginOfShellCollectionId(khalaApi)
+
+// export async function getIsOriginOfShellsInventorySet(khalaApi)
+
+// export async function getSpiritsMetadata(khalaApi)
+
+// export async function getOriginOfShellsMetadata(khalaApi, raceType)
+
+// export async function getNextNftId(khalaApi, collectionId)
+
+// export async function getPreorderIndex(khalaApi)
+
+// export async function getPreorder(khalaApi, preorderId)
+
+// export async function getOriginOfShellsInventory(khalaApi, rarityType)
+
+// export async function getFoodByOwners(khalaApi, account)
+
+// export async function getOriginOfShellFoodStats(khalaApi, eraId, collectionId, nftId)
+
+// export async function getOfficialHatchTime(khalaApi)
+
+// export async function getCanStartIncubationStatus(khalaApi)
+
+// export async function getHsOriginOfShellStartedIncubationStatus(khalaApi, collectionId, nftId)
+
+// export async function getShellcollectionId(khalaApi)
+
+// export async function getShellPartsCollectionId(khalaApi)
+
+// export async function getOriginOfShellsChosenParts(khalaApi, collectionId, nftId)
