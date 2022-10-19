59adb48aa9b5c1db1c6fba647518014fb4091c24
jasl <jasl9187@hotmail.com>
2022-10-19 14:31:48 +0800

    Bump transaction version

--- runtime/rhala/src/lib.rs
+++ runtime/rhala/src/lib.rs
@@ -150,7 +150,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_version: 1182,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
-    transaction_version: 4,
+    transaction_version: 5,
     state_version: 0,
 };
 
