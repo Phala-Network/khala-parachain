f89b9a105cef58bea8e4e116dba49aa23a2b1235
h4x3rotab <h4x3rotab@gmail.com>
2022-10-26 06:57:25 +0000

    update dev scripts

--- scripts/try-runtime.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Rhala
-# ./target/release/khala-node try-runtime --chain rhala-staging-2004 on-runtime-upgrade live -u "wss://rhala-api.phala.network:443/ws" --at 0xf83a6f466ac30169274e90ad3989f9e95252f24c1c19f0a3f63476430902c333
-
-# Khala
-./target/release/khala-node try-runtime --chain khala-staging-2004 on-runtime-upgrade live -u "wss://khala-rpc.dwellir.com:443" --at 0x333d2efc5ac202a2eeec4bd5e0a092ae7a6d30db3eec2ad439a1fe99bc3dfd70
