82eafdfc5ed5e5a255be3e629af84da2d4d51ec5
jasl <jasl9187@hotmail.com>
2022-11-26 00:49:19 +0800

    bump spec version to 1195

--- runtime/phala/src/lib.rs
+++ runtime/phala/src/lib.rs
@@ -141,7 +141,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("phala"),
     impl_name: create_runtime_str!("phala"),
     authoring_version: 1,
-    spec_version: 1191,
+    spec_version: 1195,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 4,
