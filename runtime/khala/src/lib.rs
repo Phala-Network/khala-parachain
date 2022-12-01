9e61c8ac1e600057a7e50e7eb6e633deba80e137
Index0011 <mingxuan@phala.network>
2022-12-01 06:49:38 +0000

    push

--- runtime/khala/src/lib.rs
+++ runtime/khala/src/lib.rs
@@ -159,7 +159,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("khala"),
     impl_name: create_runtime_str!("khala"),
     authoring_version: 1,
-    spec_version: 1195,
+    spec_version: 1196,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 5,
