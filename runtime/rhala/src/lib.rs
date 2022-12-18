d4e975666a65b61d285ffcd44384c2bfc0679de0
jasl <jasl9187@hotmail.com>
2022-12-18 16:05:59 +0800

    Remove staled PW migrations

--- runtime/rhala/src/lib.rs
+++ runtime/rhala/src/lib.rs
@@ -132,7 +132,6 @@ pub use pallet_sudo::Call as SudoCall;
 pub use pallet_timestamp::Call as TimestampCall;
 #[cfg(any(feature = "std", test))]
 pub use sp_runtime::BuildStorage;
-use crate::migrations::PhalaWorldRhalaMigrations;
 
 /// Opaque types. These are used by the CLI to instantiate machinery that don't need to know
 /// the specifics of the runtime. They can then be made to be agnostic over specific formats
@@ -215,7 +214,7 @@ pub type Executive = frame_executive::Executive<
 
 /// All migrations executed on runtime upgrade as a nested tuple of types implementing
 /// `OnRuntimeUpgrade`.
-type Migrations = PhalaWorldRhalaMigrations;
+type Migrations = ();
 
 type EnsureRootOrHalfCouncil = EitherOfDiverse<
     EnsureRoot<AccountId>,
