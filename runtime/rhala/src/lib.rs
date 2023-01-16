8fae9d73b2fe01300f7b32b914a53f10a1338d20
jasl <jasl9187@hotmail.com>
2023-01-17 02:12:15 +0800

    Bump Khala, Rhala, Thala spec version to 1207

--- runtime/rhala/src/lib.rs
+++ runtime/rhala/src/lib.rs
@@ -164,7 +164,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("rhala"),
     impl_name: create_runtime_str!("rhala"),
     authoring_version: 1,
-    spec_version: 1206,
+    spec_version: 1207,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 6,
--- runtime/thala/src/lib.rs
+++ runtime/thala/src/lib.rs
@@ -162,7 +162,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("thala"),
     impl_name: create_runtime_str!("thala"),
     authoring_version: 1,
-    spec_version: 1206,
+    spec_version: 1207,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 6,
