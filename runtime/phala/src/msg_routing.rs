2f1d5b51ed3d50f2c7ec510f1ce2924b022fcc7c
Index0011 <mingxuan@phala.network>
2023-03-13 12:07:59 +0000

    sync phat to phala

--- runtime/phala/src/msg_routing.rs
+++ runtime/phala/src/msg_routing.rs
@@ -34,6 +34,9 @@ impl pallet_mq::QueueNotifyConfig for MessageRouteConfig {
             PhalaRegistry::on_gk_message_received,
             PhalaComputation::on_gk_message_received,
             PhalaComputation::on_working_message_received,
+            PhalaFatContracts::on_worker_cluster_message_received,
+            PhalaFatContracts::on_cluster_message_received,
+            PhalaFatContracts::on_contract_message_received,
         };
         Ok(())
     }
