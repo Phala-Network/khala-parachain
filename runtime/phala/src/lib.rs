222d9abe793fbdeafa2604e4ddb96fc8a1da8eaf
jasl <jasl9187@hotmail.com>
2023-03-29 15:25:51 +0800

    Bump version to v0.1.22, spec versions to 1220

--- runtime/phala/src/lib.rs
+++ runtime/phala/src/lib.rs
@@ -145,7 +145,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("phala"),
     impl_name: create_runtime_str!("phala"),
     authoring_version: 1,
-    spec_version: 1215,
+    spec_version: 1220,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 5,
