f862691c6d9a355df1efd29b5aa5fdee17ff8fae
Wenfeng Wang <kalot.wang@gmail.com>
2023-04-12 16:57:10 +0800

    fix phala assetsregistry migration (#275)

--- runtime/phala/src/lib.rs
+++ runtime/phala/src/lib.rs
@@ -216,7 +216,9 @@ pub type Executive = frame_executive::Executive<
 
 /// All migrations executed on runtime upgrade as a nested tuple of types implementing
 /// `OnRuntimeUpgrade`.
-type Migrations = ();
+type Migrations = (
+    assets_registry::migration::AssetsRegistryToV3Location<Runtime>,
+);
 
 type EnsureRootOrHalfCouncil = EitherOfDiverse<
     EnsureRoot<AccountId>,
