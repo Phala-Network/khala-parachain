0ec815596720f0ced1ebd8ab0bb3811570b9d15f
tolak <kalot.wang@gmail.com>
2023-05-16 15:26:35 +0800

    enable sygma bridge in khala&phala

--- runtime/phala/src/lib.rs
+++ runtime/phala/src/lib.rs
@@ -49,6 +49,7 @@ mod msg_routing;
 
 use codec::{Decode, Encode, MaxEncodedLen};
 use cumulus_pallet_parachain_system::RelayNumberStrictlyIncreases;
+use primitive_types::U256;
 use sp_api::impl_runtime_apis;
 use sp_core::{crypto::KeyTypeId, OpaqueMetadata};
 use sp_runtime::{
@@ -108,9 +109,9 @@ use xcm_executor::{Config, XcmExecutor, traits::WithOriginFilter};
 
 pub use subbridge_pallets::{
     chainbridge, dynamic_trader::DynamicWeightTrader, fungible_adapter::XTransferAdapter, helper,
-    xcmbridge, xtransfer,
+    sygma_wrapper, xcmbridge, xtransfer,
 };
-use sygma_traits::{DepositNonce, DomainID};
+use sygma_traits::{ChainID as SygmaChainID, DepositNonce, DomainID, VerifyingContractAddress};
 
 use pallet_rmrk_core::{CollectionInfoOf, InstanceInfoOf, PropertyInfoOf, ResourceInfoOf};
 use pallet_rmrk_equip::{BaseInfoOf, BoundedThemeOf, PartTypeOf};
@@ -304,7 +305,12 @@ construct_runtime! {
         RmrkEquip: pallet_rmrk_equip::{Pallet, Call, Event<T>, Storage} = 103,
         RmrkMarket: pallet_rmrk_market::{Pallet, Call, Storage, Event<T>} = 104,
 
-        // 11x kept for Sygma bridge
+        // Sygma bridge
+        SygmaAccessSegregator: sygma_access_segregator::{Pallet, Call, Storage, Event<T>} = 111,
+        SygmaBasicFeeHandler: sygma_basic_feehandler::{Pallet, Call, Storage, Event<T>} = 112,
+        SygmaBridge: sygma_bridge::{Pallet, Call, Storage, Event<T>} = 113,
+        SygmaFeeHandlerRouter: sygma_fee_handler_router::{Pallet, Call, Storage, Event<T>} = 114,
+        SygmaWrapper: sygma_wrapper::{Pallet, Storage, Event<T>} = 115,
 
         // inDEX
         PalletIndex: pallet_index::{Pallet, Call, Storage, Event<T>} = 121,
@@ -385,6 +391,10 @@ impl Contains<RuntimeCall> for BaseCallFilter {
             // Monetary
             RuntimeCall::Balances { .. }  |
             RuntimeCall::ChainBridge { .. } |
+            RuntimeCall::SygmaAccessSegregator { .. } |
+            RuntimeCall::SygmaBasicFeeHandler { .. } |
+            RuntimeCall::SygmaFeeHandlerRouter { .. } |
+            RuntimeCall::SygmaBridge { .. } |
             RuntimeCall::XTransfer { .. } |
             // Collator
             RuntimeCall::CollatorSelection(_) | RuntimeCall::Session(_) |
@@ -1497,7 +1507,7 @@ impl xcmbridge::Config for Runtime {
 
 parameter_types! {
     pub PHALocation: MultiLocation = MultiLocation::here();
-    pub PHASygmaResourceId: [u8; 32] = hex_literal::hex!("0000000000000000000000000000000000000000000000000000000000000001");
+    pub PHASygmaResourceId: [u8; 32] = hex_literal::hex!("0000000000000000000000000000000000000000000000000000000000001000");
 }
 impl assets_registry::Config for Runtime {
     type RuntimeEvent = RuntimeEvent;
@@ -1547,9 +1557,97 @@ impl chainbridge::Config for Runtime {
     type ResourceIdGenerationSalt = ResourceIdGenerationSalt;
 }
 
+// This address is provided by Sygma bridge
+const DEST_VERIFYING_CONTRACT_ADDRESS: &str = "6CdE2Cd82a4F8B74693Ff5e194c19CA08c2d1c68";
+parameter_types! {
+    // Make sure put same value with `construct_runtime`
+    pub const SygmaAccessSegregatorPalletIndex: u8 = 111;
+    pub const SygmaBasicFeeHandlerPalletIndex: u8 = 112;
+    pub const SygmaBridgePalletIndex: u8 = 113;
+    pub const SygmaFeeHandlerRouterPalletIndex: u8 = 114;
+    // RegisteredExtrinsics here registers all valid (pallet index, extrinsic_name) paris
+    // make sure to update this when adding new access control extrinsic
+    pub RegisteredExtrinsics: Vec<(u8, Vec<u8>)> = [
+        (SygmaAccessSegregatorPalletIndex::get(), b"grant_access".to_vec()),
+        (SygmaBasicFeeHandlerPalletIndex::get(), b"set_fee".to_vec()),
+        (SygmaBridgePalletIndex::get(), b"set_mpc_address".to_vec()),
+        (SygmaBridgePalletIndex::get(), b"pause_bridge".to_vec()),
+        (SygmaBridgePalletIndex::get(), b"unpause_bridge".to_vec()),
+        (SygmaBridgePalletIndex::get(), b"register_domain".to_vec()),
+        (SygmaBridgePalletIndex::get(), b"unregister_domain".to_vec()),
+        (SygmaBridgePalletIndex::get(), b"retry".to_vec()),
+        (SygmaFeeHandlerRouterPalletIndex::get(), b"set_fee_handler".to_vec()),
+    ].to_vec();
+    pub const SygmaBridgePalletId: PalletId = PalletId(*b"sygma/01");
+    // SygmaBridgeAccount is an account for holding transferred asset collection
+    // SygmaBridgeAccount address: 5EYCAe5jLbHcAAMKvLFSXgCTbPrLgBJusvPwfKcaKzuf5X5e
+    pub SygmaBridgeAccount: AccountId = SygmaBridgePalletId::get().into_account_truncating();
+    // SygmaBridgeFeeAccountKey Address: 5FbR7ZkYSgsFsBMmqsdEpgAU7DkMdKpkGWSBZdhn6LAdesQ6
+    pub SygmaBridgeFeeAccountKey: [u8; 32] = hex::decode("9c20f205595671c6f121b9d9bbfb9caa66380f8f3ad8d47d901b84b7fb757042").unwrap().try_into().unwrap();
+    pub SygmaBridgeFeeAccount: AccountId = SygmaBridgeFeeAccountKey::get().into();
+    // EIP712ChainID is the chainID that pallet is assigned with, used in EIP712 typed data domain
+    pub EIP712ChainID: SygmaChainID = U256::from(5231);
+    // DestVerifyingContractAddress is a H160 address that is used in proposal signature verification, specifically EIP712 typed data
+    // When relayers signing, this address will be included in the EIP712Domain
+    // As long as the relayer and pallet configured with the same address, EIP712Domain should be recognized properly.
+    pub DestVerifyingContractAddress: VerifyingContractAddress = primitive_types::H160::from_slice(hex::decode(DEST_VERIFYING_CONTRACT_ADDRESS).ok().unwrap().as_slice());
+}
+
+impl sygma_access_segregator::Config for Runtime {
+    type RuntimeEvent = RuntimeEvent;
+    type BridgeCommitteeOrigin = EnsureRootOrHalfCouncil;
+    type PalletIndex = SygmaAccessSegregatorPalletIndex;
+    type Extrinsics = RegisteredExtrinsics;
+}
+
+impl sygma_basic_feehandler::Config for Runtime {
+    type RuntimeEvent = RuntimeEvent;
+    type BridgeCommitteeOrigin = EnsureRootOrHalfCouncil;
+    type PalletIndex = SygmaBasicFeeHandlerPalletIndex;
+}
+
+impl sygma_fee_handler_router::Config for Runtime {
+    type RuntimeEvent = RuntimeEvent;
+    type BridgeCommitteeOrigin = EnsureRootOrHalfCouncil;
+    type BasicFeeHandler = SygmaBasicFeeHandler;
+    type DynamicFeeHandler = ();
+    type PalletIndex = SygmaFeeHandlerRouterPalletIndex;
+}
+
+impl sygma_bridge::Config for Runtime {
+    type RuntimeEvent = RuntimeEvent;
+    type BridgeCommitteeOrigin = EnsureRootOrHalfCouncil;
+    type TransferReserveAccount = SygmaBridgeAccount;
+    type FeeReserveAccount = SygmaBridgeFeeAccount;
+    type EIP712ChainID = EIP712ChainID;
+    type DestVerifyingContractAddress = DestVerifyingContractAddress;
+    type FeeHandler = SygmaFeeHandlerRouter;
+    type AssetTransactor = XTransferAdapter<
+        CurrencyTransactor,
+        FungiblesTransactor,
+        XTransfer,
+        assets_registry::NativeAssetFilter<ParachainInfo>,
+        assets_registry::ReserveAssetFilter<
+            ParachainInfo,
+            assets_registry::NativeAssetFilter<ParachainInfo>,
+        >,
+    >;
+    type ResourcePairs = AssetsRegistry;
+    type IsReserve = assets_registry::SygmaAssetReserveChecker<ParachainInfo>;
+    type ExtractDestData = SygmaWrapper;
+    type PalletId = SygmaBridgePalletId;
+    type PalletIndex = SygmaBridgePalletIndex;
+    type DecimalConverter = assets_registry::SygmaDecimalConverter<AssetsRegistry>;
+}
+
+impl sygma_wrapper::Config for Runtime {
+    type RuntimeEvent = RuntimeEvent;
+}
+
 impl xtransfer::Config for Runtime {
     type RuntimeEvent = RuntimeEvent;
     type Bridge = (
+        sygma_wrapper::BridgeTransactImpl<Runtime>,
         xcmbridge::BridgeTransactImpl<Runtime>,
         chainbridge::BridgeTransactImpl<Runtime>,
     );
@@ -1971,8 +2069,7 @@ impl_runtime_apis! {
 
     impl sygma_runtime_api::SygmaBridgeApi<Block> for Runtime {
         fn is_proposal_executed(nonce: DepositNonce, domain_id: DomainID) -> bool {
-            // TODO: enable Sygma
-            false
+            SygmaBridge::is_proposal_executed(nonce, domain_id)
         }
     }
 
