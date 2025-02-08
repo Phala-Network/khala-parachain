cfa4d1d613a359c689e1671aa05c3113e5bb5626
oswald <oswald@dplabs.xyz>
2025-02-08 16:32:18 +0000

    add metadata feature and fix other dependencies

--- runtime/thala/build.rs
+++ runtime/thala/build.rs
@@ -17,10 +17,7 @@
 fn main() {
     #[cfg(all(feature = "std", feature = "metadata-hash"))]
     {
-        substrate_wasm_builder::WasmBuilder::new()
-            .with_current_project()
-            .export_heap_base()
-            .import_memory()
+        substrate_wasm_builder::WasmBuilder::init_with_defaults()
             .enable_metadata_hash("PHA", 12)
             .build();
     }
